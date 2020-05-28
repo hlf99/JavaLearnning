99d7ed35ff23e1e92de6785195e1ef4a2efa7c75
hlf99 <1520581165@qq.com>
2020-05-28 20:59:47 +0800

    线程、同步

---
+++ 02-Java进阶/day06_线程、同步/codes/day06-code/src/xw/Thread/demo03/Demo01Sleep.java
@@ -0,0 +1,23 @@
+package xw.Thread.demo03;
+
+/*
+    public static void sleep(long millis) :使当前正在执行的线程以指定的毫秒数暂停（暂时停止执行）。
+    毫秒数结束后，线程继续执行
+ */
+
+public class Demo01Sleep {
+    public static void main(String[] args) {
+        // 模拟秒表
+        for (int i = 1; i <= 60; i++) {
+            System.out.println(i);
+
+            // 使用Thread类的sleep方法让程序睡眠1秒钟
+            // sleep方法存在异常，需要处理
+            try {
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
